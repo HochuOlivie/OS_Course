94bba3c748a665615d8d224681d84197f10fae34
Vladik <emeralddog@list.ru>
2019-10-02 23:18:25 +0700

    5 task fix

--- 5/main.c
+++ 5/main.c
@@ -1,34 +1,69 @@
 #include "main.h"
 
 
+void addV(Vector *v, il elem) {
+    if (v->cap == 0) {
+        v->data = malloc(sizeof(il));
+        v->size = 1;
+        v->cap = 1;
+    }
+    if (v->size == v->cap) {
+        v->cap *= 2;
+        v->data = realloc(v->data, v->cap*sizeof(il));
+        v->data[v->size++] = elem;
+    }
+    else {
+        v->data[v->size++] = elem;
+    }
+}
+
+Vector v;
+
 int main() {
     int fp;
     fp = open("input.txt", O_RDONLY);
     
-    const long size = lseek(fp, 0, SEEK_END);
-    lseek(fp, 0, SEEK_SET);
+    //    const long size = lseek(fp, 0, SEEK_END);
+    //    lseek(fp, 0, SEEK_SET);
     
     char c;
-    int ll = 0, i = 0;
-    int idArr[size + 1];
-    idArr[ll++] = -1;
-    
+    int i = 0;
+    il a0;
+    a0.index = -1;
+    int ll = 0;
     while (read(fp, &c, 1) != 0) {
-        if (c == '\n')
-            idArr[ll++] = i;
+        ll++;
+        if (c == '\n') {
+            a0.len = ll;
+            addV(&v, a0);
+            a0.index = i;
+            ll = 0;
+        }
         i++;
     }
+    a0.len = ll;
+    addV(&v, a0);
     
     lseek(fp, 0, SEEK_SET);
+    
+    char* str = malloc(1);
+    int strLen = 1;
+    
     int num; scanf("%d", &num);
+
     while (num != 0) {
-        lseek(fp, idArr[num - 1] + 1, SEEK_SET);
-        read(fp, &c, 1);
-        while (c != '\n') {
-            printf("%c", c);
-            read(fp, &c, 1);
+        if (num > v.size || num < 0) {
+            fprintf(stderr, "Error: no such line\n");
+            scanf("%d", &num);
+            continue;
         }
-        printf("\n");
+        lseek(fp, v.data[num].index + 1, SEEK_SET);
+        if (strLen < v.data[num].len) {
+            str = realloc(str, v.data[num].len);
+            strLen = v.data[num].len;
+        }
+        read(fp, str, v.data[num].len);
+        fwrite(str, 1, v.data[num].len, stdout);
         scanf("%d", &num);
     }
     exit(0);
--- 5/main.h
+++ 5/main.h
@@ -5,7 +5,13 @@
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <fcntl.h>
-//typedef struct type {
-//    int iD;
-//    int len;
-//} type;
+typedef struct il {
+    int index;
+    int len;
+} il;
+typedef struct Vector {
+    il* data;
+    int cap;
+    int size;
+} Vector;
+void addV(Vector *v, il elem);
